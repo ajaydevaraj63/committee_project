31bc00a042ae12304bf9d5d090bdba9e9a74e9fc
ajay1600 <ajay.kumar@innovaturelabs.com>
2022-12-14 17:05:27 +0530

    login commit

---
+++ ui/committeproject/src/Components'/Login.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import '../css/Login.css'
+const Login = () => {
+  return (
+    <div>
+       <div class="ContainerMain bg-img ">
+
+
+<div class="row">
+    <div class="col col-12 col-sm-12 col-md-12 col-lg-12">
+
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 py-4"></div>
+
+
+            <div class="col col-4 ">
+
+
+            </div>
+
+            <div class=" col col-4 ">
+                <div class="box zoom-in-out-box" >
+
+                    <a class="btn btn-lg btn-google btn-block  btn-outline" href="#"><img
+                            src="https://img.icons8.com/color/16/000000/google-logo.png"/>
+                        <div class="row">
+                            <div class="col col-12 SignUpText">Signup Using
+                                Google</div>
+                        </div>
+                    </a>
+
+                </div>
+
+            </div>
+
+            <div class="col col-4 ">
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
+
+
+</div>
+    </div>
+  )
+}
+
+export default Login
